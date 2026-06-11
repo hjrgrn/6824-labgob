0ecf6831347d692ec9b8606cc9c2dea04b000a56
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-06-11 11:27:28 +0200

    Add LabEncoder.Encode
    Add functions: checkValue and checkType
    Add global variables: mu, errorCount and checked

--- labgob.go
+++ labgob.go
@@ -2,8 +2,17 @@ package labgob
 
 import (
 	"encoding/gob"
+	"fmt"
 	"io"
+	"reflect"
+	"sync"
+	"unicode"
+	"unicode/utf8"
 )
+
+var mu sync.Mutex
+var errorCount int // for TestCapital
+var checked map[reflect.Type]bool
 
 //
 // trying to send non-capitalized fields over RPC produces a range of
@@ -20,4 +29,56 @@ func NewEncoder(w io.Writer) *LabEncoder {
 	enc := &LabEncoder{}
 	enc.gob = gob.NewEncoder(w)
 	return enc
+}
+
+func (enc *LabEncoder) Encode(e any) error {
+	checkValue(e)
+	return enc.gob.Encode(e)
+}
+
+func checkValue(value any) {
+	checkType(reflect.TypeOf(value))
+}
+
+func checkType(t reflect.Type) {
+	k := t.Kind()
+
+	mu.Lock()
+	// only complain once, and avoid recursion.
+	if checked == nil {
+		checked = map[reflect.Type]bool{}
+	}
+	if checked[t] {
+		mu.Unlock()
+		return
+	}
+	checked[t] = true
+	mu.Unlock()
+
+	switch k {
+	case reflect.Struct:
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			rune, _ := utf8.DecodeRuneInString(f.Name)
+			if unicode.IsUpper(rune) == false {
+				// ta da
+				fmt.Printf("labgob error: lower-case field %v of %v in RPC or persist/snapshot will break your Raft\n",
+					f.Name, t.Name())
+				mu.Lock()
+				errorCount += 1
+				mu.Unlock()
+			}
+			checkType(f.Type)
+		}
+		return
+	case reflect.Slice, reflect.Array, reflect.Ptr:
+		checkType(t.Elem())
+		return
+	case reflect.Map:
+		checkType(t.Elem())
+		checkType(t.Key())
+		return
+	default:
+		return
+	}
 }
